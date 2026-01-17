aaf62d32a80f78e75941e29b29ded065e0936903
Adarsh Singh <226094322+arshmzp22-png@users.noreply.github.com>
2026-01-17 23:24:22 +0530

    add

---
+++ WhileLoop/EvenNumberOneToHun.py
@@ -0,0 +1,6 @@
+num=int(input("Enter the number:"))
+i=1
+while num>=i:
+    if(i%2==0):
+        print(i,end=" ")
+    i=i+1
---
+++ WhileLoop/ReverseOrderNumber.py
@@ -0,0 +1,6 @@
+"""print number 100 to 1"""
+i=0
+num=100
+while num>i:
+    print(num-i,end=" ")
+    i=i+1
